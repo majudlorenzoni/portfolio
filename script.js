9dc6ac0f9ed3d8336d89420c1f30dd2b3564b740
majudlorenzoni <juliamaria892@gmail.com>
2024-04-08 21:20:11 -0300

    Adição de cursos e ajustes na resposividade

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const email = document.getElementById('email');
   const linkedin = document.getElementById('linkedin');
   const github = document.getElementById('github');
-
+  const cursoGit = document.getElementById('cursoGit');
   // Obtém a largura e altura da janela do navegador em pixels
   let screenWidth = window.innerWidth;
   let screenHeight = window.innerHeight;
@@ -17,34 +17,38 @@ document.addEventListener('DOMContentLoaded', function() {
 console.log("Largura da tela: " + screenWidth + "px");
 console.log("Altura da tela: " + screenHeight + "px");
 
-  editor.onclick = function() {
-    window.open('https://github.com/majudlorenzoni/Computacao-Grafica', '_blank');
-  };
+editor.onclick = function() {
+  window.open('https://github.com/majudlorenzoni/Computacao-Grafica', '_blank');
+};
 
-  compraIngressos.onclick = function() {
-    window.open('https://github.com/majudlorenzoni/compra_online_ingressos-BD', '_blank');
+compraIngressos.onclick = function() {
+  window.open('https://github.com/majudlorenzoni/compra_online_ingressos-BD', '_blank');
   };
 
   pet.onclick = function() {
     window.open('https://wp.ufpel.edu.br/petcomp/', '_blank');
   }
-
+  
   sistemaSolar.onclick = function() {
     window.open('https://github.com/majudlorenzoni/solarSystem', '_blank');
   }
-
+  
   cursoReact.onclick = function() {
     window.open('https://comunidade.ada.tech/cursos/b60dfd96-34d9-4880-b23e-7e3679eb5391', '_blank');
   } 
-
+  
+  cursoGit.onclick = function() {
+    window.open('https://comunidade.ada.tech/cursos/37f4b5d2-dbab-4c45-ab61-aac1ba2c7d19', '_blank');
+  }
+  
   cursoIngles.onclick = function() {
     window.open('https://icl.com.br/curso/ingles-2-0/', '_blank');
   }
-
+  
   cursoAnimacao.onclick = function() {
     window.open('https://mundi.ifsul.edu.br/portal/producao-de-animacao-2d.php', '_blank');
   }
-
+  
   email.onclick = function() {
     window.open('mailto:juliamaria892@gmail.com', '_blank');
   }
@@ -52,9 +56,9 @@ console.log("Altura da tela: " + screenHeight + "px");
   linkedin.onclick = function() {
     window.open('https://www.linkedin.com/in/maria-j%C3%BAlia-lorenzoni-b09489230/', '_blank');
   }
-
+  
   github.onclick = function() {
     window.open('https://github.com/majudlorenzoni', '_blank');
   }
-
+  
 });
